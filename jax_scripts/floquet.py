91d046926b01956a3179212a17e9571060993fb6
Matthew R Golden <mgolden30@atl1-1-01-006-29-0.pace.gatech.edu>
2025-10-23 17:08:21 -0400

    fixed floquet analysis

--- jax_scripts/floquet.py
+++ jax_scripts/floquet.py
@@ -4,18 +4,30 @@ Estimate the Floquet spectrum of a Relative Periodic Orbit (RPO)
 import time
 import jax
 import jax.numpy as jnp
+import numpy as np
 
 import lib.mhd_jax as mhd_jax
+import lib.loss_functions as loss_functions
 import lib.dictionaryIO as dictionaryIO
 
 from scipy.io import savemat, loadmat
+
+
+
+
+
+input_file = "solutions/Re40/TW2.npz"
+output_file= "floquet/Re40/TW2.npz"
+
+
 
 precision = jnp.float64  # Double or single precision
 # If you want double precision, change JAX defaults
 if (precision == jnp.float64):
     jax.config.update("jax_enable_x64", True)
 
-input_dict, param_dict = dictionaryIO.load_dicts("solutions/Re50/2.npz")
+input_dict, param_dict = dictionaryIO.load_dicts(input_file)
+param_dict.update(dictionaryIO.recompute_grid_information(input_dict, param_dict))
 
 f = input_dict['fields']
 
@@ -27,13 +39,11 @@ print(f"Using fixed timesteps = {param_dict['steps']}")
 
 @jax.jit
 def forward(f):
-    dt = input_dict['T'] / param_dict['steps']
-    f = jnp.fft.rfft2(f)
-    #evolve in time
-    f = mhd_jax.eark4(f, dt, param_dict['steps'], param_dict)
-    #Translate in space
-    f = jnp.exp( -1j*param_dict['kx']*input_dict['sx'] ) * f
-    f = jnp.fft.irfft2(f)
+    mode = "TDRK4"
+    in_dict = {'fields' : f, 'T': input_dict['T'], 'sx': input_dict['sx']}
+    _, f, _ = loss_functions.mismatch_RPO(in_dict, param_dict, mode)
+    #output is in Fourier space. Transform back to real space
+    f = jnp.fft.irfft2(f * param_dict['mask'])
     return f
 
 #Try using the evolution function.
@@ -54,11 +64,11 @@ key = jax.random.PRNGKey(seed=0)
 tang = jax.random.normal( key, [block_size, 2, n, n], dtype=precision )
 
 #Or load in a previous convergence
-data = loadmat("floquet.mat")
-tang = data["tang"]
+#data = loadmat("floquet.mat")
+#tang = data["tang"]
 
 #How many times should we do power iteration?
-maxit = 8
+maxit = 256
 for i in range(maxit):
     print(tang.shape)
 
@@ -80,3 +90,11 @@ jt = jnp.reshape( jac(tang), [block_size, -1] )
 R = t @ jt.transpose()
 
 savemat("floquet.mat", {"R": R, "tang": tang, "diff": f_out - f })
+
+
+
+floquet_dict = {"R": R, "Q": tang}
+#Convert to default numpy from jnp
+data_np = {k: np.array(v) for k, v in floquet_dict.items()}
+#Save. ** does dictionary unpacking
+np.savez( output_file, **data_np )
